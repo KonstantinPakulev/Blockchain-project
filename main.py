03d18085ffbdf524539f31f3bf7659c1e0f98d8a
ndmel <ndmelentev@yandex.ru>
2018-12-17 17:25:51 +0300

    Added puzzle module, updated main to test puzzle.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from cipher.TripleDES import *
+from puzzle.puzzle import TimeLockPuzzle
 
 triple_des = TripleDES()
 
@@ -11,3 +12,11 @@ print("Key:", encoded[1] + encoded[2] + encoded[3])
 decoded = triple_des.decode(encoded[0][1], encoded[1], encoded[2], encoded[3])
 # Decoded text and its binary representation
 print(decoded)
+
+# Check puzzle
+T = 1  # sec
+S = 1000  # solver efficiency (1000000 - optimal)
+tlp = TimeLockPuzzle(T, S)
+n, a, t, Ck, Cm = tlp.generate_puzzle(string_to_encode)
+decrypted_message = tlp.solve_puzzle(n, a, t, Ck, Cm)
+print(decrypted_message)
